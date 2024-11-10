1995228e19f51e186db114764b45252eee28c58e
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2024-11-09 22:08:39 -0800

    Added Adaboost in the main file
    Adaboost does not conform with the general neural network model.
    
    As a result, using the model was added directly onto the main.py
    
    Other statistics to calculate are TBD.

--- mlproject/main.py
+++ mlproject/main.py
@@ -9,6 +9,12 @@ from pathlib import Path
 from mlproject import constants as pc
 import polars as pl
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.preprocessing import LabelEncoder
+
+import numpy as np
+
+
 @click.group()
 def cli():
     pass
@@ -44,15 +50,17 @@ def read_raw_data_common_options(func):
         return func(*args, **kwargs)
     return wrapper
 
+
 AVAILABLE_EMBEDDERS = list(ArticleEmbedder.registered_subclasses.keys())
 AVAILABLE_NN_MODELS = list(NNBaseModel.registry.keys())
+
 
 @cli.command()
 @click.option(
     "--csv-path",
-        type=click.Path(exists=True),
-        required=True,
-        help="The path to the raw data CSV file."
+    type=click.Path(exists=True),
+    required=True,
+    help="The path to the raw data CSV file."
 )
 @click.option(
     "--seed",
@@ -77,10 +85,12 @@ def get_wikihow_subset(
         nrows: int,
         subset_path: Optional[str] = None
 ):
-    wikihow_subset = WikihowSubset.return_subset(csv_path=csv_path, seed=seed, n_rows=nrows)
+    wikihow_subset = WikihowSubset.return_subset(
+        csv_path=csv_path, seed=seed, n_rows=nrows)
     if subset_path:
         print(f"Saving subset to {subset_path}")
         wikihow_subset.data.write_csv(subset_path)
+
 
 @cli.command()
 @click.option(
@@ -109,6 +119,7 @@ def get_queries(
     else:
         print("Generated queries:")
         print(queries_df.select("query"))
+
 
 @cli.command()
 @click.option(
@@ -141,6 +152,7 @@ def load_pretrained(
         embedder_file = Path(f"{name.lower()}.model").resolve().as_posix()
     print(f"Saving model: {name!r} to location: {embedder_file}")
     embedder.save(embedder_file)
+
 
 @cli.command()
 @click.option(
@@ -200,7 +212,8 @@ def embed_data_with_infersent(
 
     # Load the InferSent embedder
     print("Loading the InferSent embedder...")
-    embedder = InferSentEmbedder(model=None, vector_size=vector_size, model_path=model_path,w2v_path=w2v_path)
+    embedder = InferSentEmbedder(
+        model=None, vector_size=vector_size, model_path=model_path, w2v_path=w2v_path)
     embedder.load_model()
 
     # Load the dataset
@@ -216,7 +229,8 @@ def embed_data_with_infersent(
     # Embed the data
     print("Starting to embed data...")
 
-    embeddings = HumanChatBotDataset.from_raw_data(dataset, embedder)#embedder.embed_articles(dataset)
+    embeddings = HumanChatBotDataset.from_raw_data(
+        dataset, embedder)  # embedder.embed_articles(dataset)
 
     # Create a DataFrame with embeddings and indices
     embeddings_df = embeddings.data
@@ -225,6 +239,7 @@ def embed_data_with_infersent(
     print(f"Saving embeddings to {embedded_path}...")
     embeddings_df.write_csv(embedded_path)
     print("Embeddings saved successfully.")
+
 
 @cli.command()
 @read_raw_data_common_options
@@ -276,8 +291,6 @@ def train_infersent_embedder(csv_path: str, n_rows: Optional[int], seed: int, tr
     embedder.save(embedder_file)
 
 
-
-
 @cli.command()
 @read_raw_data_common_options
 @click.option(
@@ -305,12 +318,14 @@ def train_embedder(
     vector_size: int,
     embedder_file: str
 ):
+    print("Loading the datasets...")
     train_data, _ = RawHumanChatBotData.train_test_split(
         path=csv_path,
         seed=seed,
         n_rows=n_rows,
         train_percent=train_percent
     )
+    print("Getting the embedder class and training the embedder...")
     embedder_klass = ArticleEmbedder.registered_subclasses[name]
     embedder = embedder_klass.by_training_on_raw_data(
         training_data=train_data,
@@ -415,6 +430,41 @@ def train_nn_model(
         epochs=epochs,
         learning_rate=learning_rate
     )
+
+
+@cli.command()
+@click.option(
+    "--training-dataset-path",
+    type=click.Path(exists=True, dir_okay=False),
+    required=True
+)
+@click.option(
+    "--testing-dataset-path",
+    type=click.Path(exists=True, dir_okay=False),
+    required=True
+)
+def adaboost(training_dataset_path: str, testing_dataset_path: str):
+    print("Loading datasets...")
+    train_dataset = HumanChatBotDataset.load(training_dataset_path)
+    test_dataset = HumanChatBotDataset.load(testing_dataset_path)
+    train_df = train_dataset.data
+    test_df = test_dataset.data
+    print("Adapting data for scikit-learn...")
+    train_X = train_df.select(pl.col("*").exclude(["type", "label"]))
+    _train_Y = train_df.select("type").to_numpy().ravel()
+    test_X = test_df.select(pl.col("*").exclude(["type", "label"]))
+    _test_Y = test_df.select("type").to_numpy().ravel()
+    le = LabelEncoder()
+    n_training = len(_train_Y)
+    all_y = np.concatenate((_train_Y, _test_Y))
+    all_y = le.fit_transform(all_y)
+    train_Y = all_y[:n_training]
+    test_Y = all_y[n_training:]
+    print("Training the model...")
+    model = AdaBoostClassifier(algorithm="SAMME")
+    model.fit(train_X, train_Y)
+    accuracy = model.score(test_X, test_Y)
+    print(f"Accuracy: {accuracy*100:.4f}%")
 
 
 def main():
